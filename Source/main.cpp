90862adfaebfb03ed8df098b13b79de78fcc91fb
manjogsingh <manjogsingh@gmail.com>
2018-06-26 17:44:55 +0530

    Initial directory structure with vscode settings

---
+++ Source/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+	if (!glfwInit())
+	{
+		printf("GLFW initialisation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	return 0;
+}
